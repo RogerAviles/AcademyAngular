4d3c8d0f9f8a1a26744c323112ce310ed3a17a79
RogerAviles <rogeravigar_11@hotmail.com>
2020-10-20 10:33:22 -0500

    Aplicando Angular de StackBlitz a Escritorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { IndexComponent } from './secondary/index/index.component';
+import { NosotrosComponent } from './secondary/nosotros/nosotros.component';
+import { PricingComponent } from './secondary/pricing/pricing.component';
 
+const rutas: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'nosotros', component: NosotrosComponent},
+  { path: 'precios', component: PricingComponent}
+ ];
+ 
 @NgModule({
   declarations: [
-    AppComponent
+    NavbarComponent,
+    AppComponent,
+    FooterComponent,
+    IndexComponent,
+    NosotrosComponent,
+    PricingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
