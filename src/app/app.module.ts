1ef2099f7a92edc51b906695166dc0c837855a47
RogerAviles <rogeravigar_11@hotmail.com>
2020-11-05 18:11:32 -0500

    Agregado componente about; agregado flaticon; cambios navbar; agregado MODAL para login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,12 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { IndexComponent } from './secondary/index/index.component';
 import { NosotrosComponent } from './secondary/nosotros/nosotros.component';
 import { PricingComponent } from './secondary/pricing/pricing.component';
+import { AboutComponent } from './secondary/about/about.component';
+import { LoginComponent } from './shared/login/login.component';
 
 
 //https://fontawesome.bootstrapcheatsheets.com/
+//https://www.lwtech.edu/_resources/fonts/flat-icons/fonts-simpleicon/flaticon.html
 
 const rutas: Routes = [
   { path: '', component: IndexComponent },
@@ -26,7 +29,9 @@ const rutas: Routes = [
     FooterComponent,
     IndexComponent,
     NosotrosComponent,
-    PricingComponent
+    PricingComponent,
+    AboutComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
